08c082add43373157eeb0acacf7b823af873456b
R4y <quartz010@outlook.com>
2018-01-04 23:56:18 +0800

    dbg 信息优化

--- Bkjwer/main.py
+++ Bkjwer/main.py
@@ -12,6 +12,10 @@ class Bkjw:
     def __init__(self):
         self.session = requests.session()
         self.islogedin = False
+        try:
+            requests.get(self.root_url, timeout=1)
+        except requests.exceptions.ConnectTimeout:
+            print("[-] Network Unreachable")
 
     # 要提交的键值对的一个结构
     keywords = {"username": "1500110428", "passwd": "529160", 'login': '%B5%C7%A1%A1%C2%BC'}
@@ -29,20 +33,27 @@ class Bkjw:
     }
 
     def login(self, url, keywords):
-
         # 以post的方式提交表单并保存结果在变量res中
-        res = self.session.post(url + self.sub_url_tab["url_login"], data=keywords)
-        print(res.reason)
+        try:
+            res = self.session.post(url + self.sub_url_tab["url_login"], data=keywords, json=None, timeout = 0.5)
+        #  这里对超时进行异常处理
+        except requests.exceptions.ConnectTimeout:
+            self.islogedin = False
+            print('[-] Connect Timeout!')
+            return
 
         res = self.session.get(url + self.sub_url_tab["url_info"])
         # 验证是否成功登陆
         if res.text.find(keywords["username"]) > 0:
-            print('OK')
+            print('[+] Logged in')
             self.islogedin = True
         else:
             print("Session Create Error!")
 
     def logout(self):
+        if not self.islogedin:
+            return
+
         self.session.get(self.sub_url_tab["url_logout"])
         self.session.close()
 
@@ -74,12 +85,19 @@ class Bkjw:
         # for d in courses_list:
         #     print(d)
 
-        # res = self.session.get("http://172.16.64.236/student/Selected.asp")
-        # print(res.text)
+        res = self.session.get(self.root_url + self.sub_url_tab["url_courses"])
+        print(res.text)
 
-    def listout(self, infoset):
-        for d in infoset:
-            print(d)
+    def listout(self, info_set):
+        """
+            这里用于列出爬到的set
+        """
+        
+        if info_set is not None:
+            for d in info_set:
+                print(d)
+        else:
+            print('[-] None Type !')
 
 
 def connect_test():
@@ -102,4 +120,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
 
